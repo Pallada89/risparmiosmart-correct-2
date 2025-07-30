6be259d7c196da0ca574d79747327452c391b4c2
Pallada89 <alessandropalladino889@gmail.com>
2025-07-30 15:02:51 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>RisparmioSmart</h1>
+      <p>App base React pronta per Vercel</p>
+    </div>
+  );
+}
+
+export default App;
